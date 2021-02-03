ccbce6acf379a0baaa3e0d54fbae76a7797ff87d
Eric Bartusch <eric.bartusch@genmills.com>
2021-02-03 12:50:07 -0600

    beginnings of auth call

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const core = require("@actions/core");
+const { request } = require('./httpClient');
+const { GithubActions } = require('./githubActions');
+
+async function main() {
+  try {
+    // inputs from action
+    const vaultUrl = core.getInput('vaultUrl', { required: true });
+    const roleId = core.getInput('roleId', { required: true });
+    const secretId = core.getInput('secretId', { required: true });
+    const rolesetPath = core.getInput('rolesetPath', { required: true });
+    const vaultAuthPayload = `{
+      "role_id": "${roleId}",
+      "secret_id": "${secretId}"
+    }`;
+    
+    // current time
+    const time = new Date().toTimeString();
+
+    // authenticate to vault
+    const response = await request(
+      `${vaultUrl}/v1/auth/approle/login`,
+      "POST",
+      vaultAuthPayload,
+    );
+
+    const statusCode = response.status;
+    const data = response.data;
+    const outputObject = {
+      url,
+      method,
+      payload,
+      time,
+      statusCode,
+      data
+    };
+
+    const consoleOutputJSON = JSON.stringify(outputObject, undefined, 2);
+    console.log(consoleOutputJSON);
+
+    if (statusCode >= 400) {
+      core.setFailed(`HTTP request failed with status code: ${statusCode}`);
+    } else {
+      const outputJSON = JSON.stringify(outputObject);
+      core.setOutput('output', outputJSON);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
