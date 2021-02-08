327d527b816cdab3ece403481e0a78ffa108cb64
Eric Bartusch <ericbartusch8@gmail.com>
2021-02-08 10:40:38 -0600

    babiest steps

--- dist/index.js
+++ dist/index.js
@@ -3057,7 +3057,7 @@ async function main() {
 
     var statusCode = authResponse.status;
     var data = authResponse.data;
-    console.log(data.auth);
+    console.log(data.auth.client_token);
     const vaultToken = data.client_token;
 
     //     // get GCP Service Account private key from Vault
