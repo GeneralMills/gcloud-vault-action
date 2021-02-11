266a6505c83da77764231f53ce98007f23f75c15
Eric Bartusch <ericbartusch8@gmail.com>
2021-02-11 09:38:45 -0600

    wow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const request  = require('./httpClient');
 const fs = require('fs');
-const { execSpawn } = require("child_process");
+const { spawnSync } = require("child_process");
 const { exception } = require("console");
 
 async function main() {
@@ -27,7 +27,7 @@ async function main() {
     });
 
     // auth to GCP with service account
-    execSpawn('gcloud auth activate-service-account --key-file sa-key.json', (error, stdout, stderr) => {
+    spawnSync('gcloud auth activate-service-account --key-file sa-key.json', (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         throw error;
@@ -38,7 +38,7 @@ async function main() {
 
     // execute provided command
     console.log(`Executing command: ${gcloudCommand}`);
-    execSpawn(gcloudCommand, (error, stdout, stderr) => {
+    spawnSync(gcloudCommand, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         throw error;
